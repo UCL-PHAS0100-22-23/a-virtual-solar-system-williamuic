b0becd6fbce1cdcfec507e0c8575577376b92370
William <ucaphwe@ucl.ac.uk>
2023-04-01 08:50:52 +0000

    function implementation for 1.1.1 Creating the particle class

--- src/particle.cpp
+++ src/particle.cpp
@@ -1,10 +1,31 @@
 #include "particle.hpp"
 #include <Eigen/Core>
 
-Particle::Particle(double in_mass) :
-    mass{in_mass}
-    {}
+Particle::Particle(const Eigen::Vector3d& in_position, const Eigen::Vector3d& in_velocity, const Eigen::Vector3d& in_acceleration, double in_mass)
+    : position(in_position), velocity(in_velocity), acceleration(in_acceleration), mass(in_mass) {}
+
+Eigen::Vector3d Particle::getPosition() const {
+    return position;
+}
+
+Eigen::Vector3d Particle::getVelocity() const {
+    return velocity;
+}
+
+Eigen::Vector3d Particle::getAcceleration() const {
+    return acceleration;
+}
 
 double Particle::getMass() const {
     return mass;
+}
+
+void Particle::update(double dt) {
+    position += dt * velocity;
+    velocity += dt * acceleration;
+}
+
+
+void Particle::setAcceleration(const Eigen::Vector3d& in_acceleration) {
+    acceleration = in_acceleration;
 }
