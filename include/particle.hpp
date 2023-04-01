fffd882f0131a5b72ba2ece1bf40f005c38510f3
William <ucaphwe@ucl.ac.uk>
2023-04-01 08:50:06 +0000

    function declaration for 1.1.1 Creating particle class

--- include/particle.hpp
+++ include/particle.hpp
@@ -1,8 +1,27 @@
-class Particle {
-    public:
-        Particle(double in_mass);
-        double getMass() const;
+#ifndef PARTICLE_HPP
+#define PARTICLE_HPP
 
-    private:
-        double mass;
+#include <Eigen/Core>
+
+class Particle {
+public:
+    Particle(const Eigen::Vector3d& in_position, const Eigen::Vector3d& in_velocity, const Eigen::Vector3d& in_acceleration, double in_mass);
+
+    Eigen::Vector3d getPosition() const;
+    Eigen::Vector3d getVelocity() const;
+    Eigen::Vector3d getAcceleration() const;
+    double getMass() const;
+
+    void setAcceleration(const Eigen::Vector3d& in_acceleration);
+    void update(double dt);
+     
+    void SumAccelerations(const std::vector<Particle>& particles, double epsilon);
+ 
+
+private:
+    Eigen::Vector3d position;
+    Eigen::Vector3d velocity;
+    Eigen::Vector3d acceleration;
+    double mass;
 };
+#endif
