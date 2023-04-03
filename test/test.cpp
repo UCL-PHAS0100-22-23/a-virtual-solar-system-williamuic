afa262db4a06687ed5aeb51aeb5a9053df6b6a58
William <ucaphwe@ucl.ac.uk>
2023-04-03 21:08:59 +0000

    add test for simulator and initial generator

--- test/test.cpp
+++ test/test.cpp
@@ -3,8 +3,34 @@
 #include "particle.hpp"
 #include <Eigen/Core>
 #include <iostream>
+#include <random>
 using Catch::Matchers::WithinRel;
+std::vector<Particle> create_initial_particles() {
+    std::vector<double> masses = {1.0, 1.0/6023600, 1.0/408524, 1.0/332946.038, 1.0/3098710, 1.0/1047.55, 1.0/3499, 1.0/22962, 1.0/19352};
+    std::vector<double> distances = {0.0, 0.4, 0.7, 1.0, 1.5, 5.2, 9.5, 19.2, 30.1};
+    std::vector<Particle> particles;
+    particles.reserve(masses.size());
+    //Developer could choose using rd or use a fix number so that every time it will generate same random number
+    std::random_device rd;
+    std::mt19937 gen(80);
+    std::uniform_real_distribution<> dist(0, 2 * M_PI);
+    // Create particle for the Sun
+    Eigen::Vector3d position(0.0, 0.0, 0.0);
+    Eigen::Vector3d velocity(0.0, 0.0, 0.0);
+    Eigen::Vector3d acceleration(0.0, 0.0, 0.0);
+    particles.emplace_back(position, velocity, acceleration, masses[0]);
+    // Create particles for each planet
+    for (size_t i = 1; i < masses.size(); ++i) {
+        double theta = dist(gen);
+        double r = distances[i];
 
+        Eigen::Vector3d position(r * sin(theta), r * cos(theta), 0.0);
+        Eigen::Vector3d velocity(-sqrt(1.0 / r) * cos(theta), sqrt(1.0 / r) * sin(theta), 0.0);
+        Eigen::Vector3d acceleration(0.0, 0.0, 0.0);
+        particles.emplace_back(position, velocity, acceleration, masses[i]);
+    }
+    return particles;
+}
 TEST_CASE("Particle moves with no acceleration", "[particle]") {
     Particle p(Eigen::Vector3d(1.0, 2.0, 3.0), Eigen::Vector3d(4.0, 5.0, 6.0), Eigen::Vector3d(0.0, 0.0, 0.0), 1.0);
     double dt = 0.1;
@@ -128,4 +154,38 @@ TEST_CASE("SumAccelerations edge cases", "[particle]") {
     Eigen::Vector3d expected_acceleration_p4 = Eigen::Vector3d(-1e30, 0.0, 0.0);
     Eigen::Vector3d expected_acceleration_p5 = Eigen::Vector3d(5e29, 0.0, 0.0);
     REQUIRE(p3.getAcceleration().isApprox(expected_acceleration, 1e-5));
+}
+
+//1.3 Simulator
+TEST_CASE("create_initial_particles with correct number of particles", "[particle]") {
+    std::vector<Particle> particles = create_initial_particles();
+    REQUIRE(particles.size() == 9);
+}
+
+TEST_CASE("create_initial_particles with correct masses", "[particle]") {
+    std::vector<Particle> particles = create_initial_particles();
+    std::vector<double> expected_masses = {1.0, 1.0/6023600, 1.0/408524, 1.0/332946.038, 1.0/3098710, 1.0/1047.55, 1.0/3499, 1.0/22962, 1.0/19352};
+    for (size_t i = 0; i < expected_masses.size(); ++i) {
+        REQUIRE(particles[i].getMass() == expected_masses[i]);
+    }
+}
+
+TEST_CASE("single body orbits around the Sun", "[solarSystem]") {
+    // Set up a single particle orbiting around the Sun
+    Particle p1(Eigen::Vector3d(0.0, 0.0, 0.0), Eigen::Vector3d::Zero(), Eigen::Vector3d::Zero(), 1.0);
+    Particle p2(Eigen::Vector3d(1.0, 0.0, 0.0), Eigen::Vector3d(0.0, 1.0, 0.0), Eigen::Vector3d::Zero(), 1e-6);
+    std::vector<Particle> particles = {p1, p2};
+    int num_steps = 1;
+    int dt = 0.0001;
+    // Run the simulation
+    for (int step = 0; step < num_steps; ++step) {
+        for (Particle& p : particles) {
+            p.SumAccelerations(particles, 1e-5);
+        }
+        for (Particle& p : particles) {
+            p.update(dt);
+        }
+    }
+    Eigen::Vector3d expected_pos(1, 0, 0);
+    REQUIRE(particles[1].getPosition().isApprox(expected_pos, 1e-6));
 }
